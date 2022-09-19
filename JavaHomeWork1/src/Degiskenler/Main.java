92ac60de9c96776c22123fde84cf34ecbe993da7
betulkoyunccu <113598275+betulkoyunccu@users.noreply.github.com>
2022-09-19 09:35:12 +0300

    First Commit

---
+++ JavaHomeWork1/src/Degiskenler/Main.java
@@ -0,0 +1,11 @@
+package Degiskenler;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int ogrenciSayisi = 10;
+		String mesaj = "Öðrenci sayýsý : ";
+		System.out.println(mesaj+ ogrenciSayisi);
+	}
+}
