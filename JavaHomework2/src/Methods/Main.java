3b7a6e74b04a3fc4e0a207c55b3f33620fb9434e
betulkoyunccu <113598275+betulkoyunccu@users.noreply.github.com>
2022-09-23 17:35:05 +0300

    First commit

---
+++ JavaHomework2/src/Methods/Main.java
@@ -0,0 +1,34 @@
+package Methods;
+
+public class Main {
+
+	public static void main(String[] args) {
+		sayiBulmaca();// methodu çagirdik
+
+	}
+
+	public static void sayiBulmaca() {// method
+
+		int[] sayilar = new int[] { 1, 2, 5, 7, 9 };
+		int aranacak = 6;
+
+		boolean varMi = false;
+
+		for (int sayi : sayilar) {
+			if (sayi == aranacak) {
+				varMi = true;
+				break;
+			}
+		}
+		if (varMi) {
+			mesajVer("Sayý Mevcuttur: " + aranacak);
+		} else {
+			mesajVer("Sayý Mevcut degildir: " + aranacak);
+		}
+	}
+
+	// public static void mesajVer(int aranacak) {//Parametreli method
+	public static void mesajVer(String mesaj) {
+		System.out.println(mesaj);
+	}
+}
